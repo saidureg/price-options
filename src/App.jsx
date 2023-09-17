b30a75eec4ae4172968b4f904583dadf699769c4
saidureg <saidureg@gmail.com>
2023-09-17 22:29:30 +0600

    created a responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import DaisyNavbar from "./components/daisyNavbar/DaisyNavbar";
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
+      <DaisyNavbar></DaisyNavbar>
       <h1 className="text-3xl bg-rose-700">Price Option</h1>
     </>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Link from "../link/Link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const routes = [
+    { id: 1, name: "Home", path: "/" },
+    { id: 2, name: "About", path: "/about" },
+    { id: 3, name: "Contact", path: "/contact" },
+    { id: 4, name: "Products", path: "/products" },
+    { id: 5, name: "Not Found", path: "*" },
+  ];
+
+  return (
+    <div>
+      <div className="text-2xl md:hidden" onClick={() => setOpen(!open)}>
+        {open === true ? (
+          <AiOutlineClose></AiOutlineClose>
+        ) : (
+          <AiOutlineMenu></AiOutlineMenu>
+        )}
+      </div>
+      <ul className="md:flex">
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
